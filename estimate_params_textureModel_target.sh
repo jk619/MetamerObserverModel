5488fc544b35f36ce75c4292390005574c4d8c00
jk619 <jan.kurzawski@gmail.com>
2024-01-26 13:22:56 -0500

    adjust to jaw288

--- estimate_params_textureModel_target.sh
+++ estimate_params_textureModel_target.sh
@@ -5,9 +5,9 @@
 #SBATCH --cpus-per-task=16 #~2 days to run PRFs
 #SBATCH --mem=128gb # More memory you request the less priority you get
 #SBATCH --time=50:00:00 # Max request to be safe...
-#SBATCH --output=/scratch/jk7127/logs/param_tar_est_out_ses_%x-%a.txt # Define output log location
-#SBATCH --error=/scratch/jk7127/logs/param_tar_est_err_ses_%x-%a.txt # and the error logs for when it inevitably crashes
-#SBATCH --mail-user=jk7127@nyu.edu #email
+#SBATCH --output=/scratch/jaw288/logs/param_tar_est_out_ses_%x-%a.txt # Define output log location
+#SBATCH --error=/scratch/jaw288/logs/param_tar_est_err_ses_%x-%a.txt # and the error logs for when it inevitably crashes
+#SBATCH --mail-user=jaw288@nyu.edu #email
 #SBATCH --mail-type=END #email me when it crashes or better, ends
 
 #all_subjects=(ALESPA ANDBAS ANDTOR ANTBRA ARIZER ASISOR CALCAR CAMCAP CHISOL CHITOR CLALAV CLANUT DANDAC ELECHE EMAMAM FABGUA GIOTRI IVAPI MIRACQ NOVNAR PAOCON PIEAMB SARCOP TOMBIA VERTUL)
