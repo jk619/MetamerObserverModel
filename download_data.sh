33753f645601a5b8950f0499c2b66b9b01bef3cf
jk619 <jan.kurzawski@gmail.com>
2024-01-26 20:02:51 -0500

    remove png

--- download_data.sh
+++ download_data.sh
@@ -3,7 +3,7 @@ curl -O -J -L https://osf.io/ytmvq/download
 gunzip metamers_energy_met.tar.gz
 tar -xvf metamers_energy_met.tar
 
-curl -O -J -L https://osf.io/ytmvq/download
+curl -O -J -L https://osf.io/c4gaw/download
 gunzip metamers_energy_ref.tar.gz
 tar -xvf metamers_energy_ref.tar
 
@@ -15,10 +15,10 @@ rm metamers_energy_ref.tar
 rm metamers_energy_met.tar
 
 curl -O -J -L https://osf.io/hv437/download
-gunzip tiff_images.tar.gz
-tar -xvf tiff_images.tar
+gunzip target_images.tar.gz
+tar -xvf target_images.tar --one-top-level
 
-rm tiff_images.tar
+rm target_images.tar
 
 
 # create log directory under scratch
